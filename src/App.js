c401157df7aac5cb28ccfd6a4f265886a56a0219
Minh Hiếu <hieudm05@gmail.com>
2025-03-17 17:07:13 +0700

    setState

--- src/App.js
+++ src/App.js
@@ -3,27 +3,35 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Xin chào
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-    <div>
-      Heloo 
-      <MyComponent/>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Heloo
+        <MyComponent />
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Xin chào
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,37 @@
 // class components
 import React from "react";
 
-class MyComponent extends React.Component{
+class MyComponent extends React.Component {
+    state = {
+        name: "Minh Hiếu",
+        address: "Ninh Bình",
+        age: 20
+    };
+
+    handleClick = (event) =>{
+        // console.log("click");
+        console.log(this.state.name);
+        // merge State => react class
+        this.setState({
+            name: "Minh Hiếu 2",
+            age: Math.floor((Math.random() * 100)) + 1
+
+        })
+        
+    }
+    handleOnMouverOver (event){
+        console.log(event);
+        
+    }
     // JSX
-    render(){
-        return(
+    render() {
+        return (
             <div>
-               {Math.random()}
+                Tôi tên là {this.state.name}
+                Tuổi {this.state.age}
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
+                <button onMouseOver={this.handleOnMouverOver}>Click 2</button>
+
             </div>
         )
     }
