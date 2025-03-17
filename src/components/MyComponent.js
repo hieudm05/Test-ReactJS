011eb36370edf1f44455b6ace01ad82bad016620
Minh Hiếu <hieudm05@gmail.com>
2025-03-17 15:53:13 +0700

    components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+// class components
+import React from "react";
+
+class MyComponent extends React.Component{
+    // JSX
+    render(){
+        return(
+            <div>
+               {Math.random()}
+            </div>
+        )
+    }
+}
+export default MyComponent
