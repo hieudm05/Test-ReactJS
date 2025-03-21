9cf25ae56204fe2b6379dbd10357921bf7b85b01
Minh Hiếu <hieudm05@gmail.com>
2025-03-21 14:16:11 +0700

    useState Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,64 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
-class DisplayInfor extends React.Component {
-    constructor(props) {
-        console.log("call contructer: 1")
-        super(props);
-        this.state = {
-            isShow: true
-        }
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShow: !this.state.isShow
-        })
-    }
-    componentDidMount() {
-        console.log("call me component did mount")
-        setTimeout(() => {
-            document.title = "Test"
-        },2000)
-    }
-    componentDidUpdate(prevProps, prevState, snapshot){
-        console.log("Call update",this.props, prevProps);
-        if(this.props.listUser  !== prevProps.listUser){
-            if(this.props.listUser.length === 2){
-                alert("Bạn có 2 users")
-            }
-        }
-    }
+// class DisplayInfor extends React.Component {
+//     render() {
+//         console.log(this.props);
+//         const { listUser } = this.props; //Obj
+//         console.log("call me render")
+//         // console.log(listUser);
+//         return (
+//             <div className="display-info-container">
+//                 {/* <img src={logo} alt=""/> */}
+//                 <div>
+//                     {true &&
+//                         <div>
+//                             {listUser.map((user) => {
+//                                 //Cách convert từ chuỗi sang number
+//                                 let check = +user.age < 21 ? "red" : "green";
+//                                 return (
+//                                     <div key={user.id} className={check}>
+//                                         <div style={{ color: "blue", paddingTop: "50px" }}>My name {user.name} </div>
+//                                         <div>Age {user.age}</div>
+//                                         <button onClick={id => this.props.handleDeleteUser(user.id)}>X</button>
+//                                         <hr />
+//                                     </div>
+//                                 )
+//                             })}
+//                         </div>}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
-    render() {
-        console.log(this.props);
-        const { listUser } = this.props; //Obj
-        console.log("call me render")
-        // console.log(listUser);
-        return (
-            <div className="display-info-container">
-                {/* <img src={logo} alt=""/> */}
-                <div>
-                    <span onClick={() => { this.handleShowHide() }}>
-                        {this.state.isShow === true ? "Hide list user:" : "Show now"}
-                    </span>
-                    {this.state.isShow &&
-                        <div>
-                            {listUser.map((user) => {
-                                //Cách convert từ chuỗi sang number
-                                let check = +user.age < 21 ? "red" : "green";
-                                return (
-                                    <div key={user.id} className={check}>
-                                        <div style={{ color: "blue", paddingTop: "50px" }}>My name {user.name} </div>
-                                        <div>Age {user.age}</div>
-                                        <button onClick={id => this.props.handleDeleteUser(user.id)}>X</button>
-                                        <hr />
-                                    </div>
-                                )
-                            })}
-                        </div>}
-                </div>
-            </div>
-        )
+const DisplayInfor = (props) => {
+    const { listUser } = props; //Obj
+    const [isShowList, setShowHide] = useState(true);
+    const handleShowHide = () =>{
+        setShowHide(!isShowList)
     }
+    return (
+        <div className="display-info-container">
+            <div>
+                <span onClick={() => handleShowHide()}>
+                    {isShowList === true ? "hide list" : "show list"}
+                </span>
+            </div>
+            <div>
+                {isShowList &&
+                    <div>
+                        {listUser.map((user) => {
+                            //Cách convert từ chuỗi sang number
+                            let check = +user.age < 21 ? "red" : "green";
+                            return (
+                                <div key={user.id} className={check}>
+                                    <div style={{ color: "blue", paddingTop: "50px" }}>My name {user.name} </div>
+                                    <div>Age {user.age}</div>
+                                    <button onClick={id => props.handleDeleteUser(user.id)}>X</button>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                    </div>}
+            </div>
+        </div>
+    )
 }
 export default DisplayInfor
