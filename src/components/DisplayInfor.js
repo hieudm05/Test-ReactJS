13b7890c7b37ecb7a1bafe92383eab4eaeaa9065
Minh Hiếu <hieudm05@gmail.com>
2025-03-21 11:43:36 +0700

    componentDidUpdate,componentDidMount

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,22 +2,41 @@ import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 class DisplayInfor extends React.Component {
-    state = {
-        isShow: true
+    constructor(props) {
+        console.log("call contructer: 1")
+        super(props);
+        this.state = {
+            isShow: true
+        }
     }
     handleShowHide = () => {
         this.setState({
             isShow: !this.state.isShow
         })
     }
+    componentDidMount() {
+        console.log("call me component did mount")
+        setTimeout(() => {
+            document.title = "Test"
+        },2000)
+    }
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log("Call update",this.props, prevProps);
+        if(this.props.listUser  !== prevProps.listUser){
+            if(this.props.listUser.length === 2){
+                alert("Bạn có 2 users")
+            }
+        }
+    }
+
     render() {
         console.log(this.props);
         const { listUser } = this.props; //Obj
+        console.log("call me render")
         // console.log(listUser);
         return (
-
             <div className="display-info-container">
-            {/* <img src={logo} alt=""/> */}
+                {/* <img src={logo} alt=""/> */}
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShow === true ? "Hide list user:" : "Show now"}
@@ -29,16 +48,15 @@ class DisplayInfor extends React.Component {
                                 let check = +user.age < 21 ? "red" : "green";
                                 return (
                                     <div key={user.id} className={check}>
-                                        <div style={{color: "blue",paddingTop: "50px"}}>My name {user.name} </div>
+                                        <div style={{ color: "blue", paddingTop: "50px" }}>My name {user.name} </div>
                                         <div>Age {user.age}</div>
-                                        <button onClick={id => this.props.handleDeleteUser(user.id) }>X</button>
+                                        <button onClick={id => this.props.handleDeleteUser(user.id)}>X</button>
                                         <hr />
                                     </div>
                                 )
                             })}
                         </div>}
                 </div>
-
             </div>
         )
     }
