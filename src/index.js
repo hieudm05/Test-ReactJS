604f04853835ec2642379a82a9cae1623f6b1e1b
hieudm05 <hieudm05@gmail.com>
2025-05-06 16:52:57 +0700

    redux-persist

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
-import 'nprogress/nprogress.css'
-import Layout from './Layout';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import "nprogress/nprogress.css";
+import Layout from "./Layout";
+import { PersistGate } from "redux-persist/integration/react";
 
 // import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-    <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 reportWebVitals();
--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -25,6 +25,8 @@ instance.interceptors.response.use(function (response) {
     // Do something with response data
     return response && response.data ? response.data : response;
   }, function (error) {
+    nProgress.done();
+
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     
