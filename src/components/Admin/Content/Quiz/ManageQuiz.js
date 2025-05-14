aac39cf160becb662b8a4c1cfadce1b9dfff27d4
hieudm05 <hieudm05@gmail.com>
2025-05-14 16:58:40 +0700

    post create quiz

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,6 +1,8 @@
 import Select from "react-select";
 import "./ManageQuiz.scss";
 import { useState } from "react";
+import { postCreateNewQuiz } from "../../../../services/apiServices";
+import { toast } from "react-toastify";
 
 const options = [
   { value: "Easy", label: "Easy" },
@@ -10,11 +12,34 @@ const options = [
 const MagageQuiz = (props) => {
   const [name, setName] = useState("");
   const [description, setDecription] = useState("");
-  const [type, setType] = useState("Easy");
+  const [type, setType] = useState("");
   const [image, setImage] = useState(null);
-  const handleChangeFile = (event) =>{
-
-  }
+  const handleChangeFile = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setImage(event.target.files[0]);
+    }
+  };
+  const handleSubmitQuiz = async () => {
+    //validate
+    if (!name) {
+      toast.error("Name is required");
+      return
+    }
+    if (!description) {
+      toast.error("Description is required");
+      return
+    }
+    let res = await postCreateNewQuiz(description, name, type?.value, image);
+    console.log("log res", res);
+    if (res && res.EC === 0) {
+      toast.success(res.EM);
+      setName("")
+      setDecription("")
+      setImage(null);
+    } else {
+      toast.error(res.EM);
+    }
+  };
   return (
     <div className="quiz-contaier container">
       <section className="title"> Magage Quiz</section>
@@ -31,7 +56,7 @@ const MagageQuiz = (props) => {
               value={name}
               onChange={(event) => setName(event.target.value)}
             />
-            <label for="floatingInput">Name</label>
+            <label htmlFor="floatingInput">Name</label>
           </div>
           <div className="form-floating">
             <input
@@ -42,21 +67,29 @@ const MagageQuiz = (props) => {
               value={description}
               onChange={(event) => setDecription(event.target.value)}
             />
-            <label for="floatingPassword">Decription</label>
+            <label htmlFor="floatingPassword">Decription</label>
             <div className="my-3">
               <Select
-                value={type}
+                defaultValue={type}
+                onChange={setType}
                 // onChange={this.handleChange}
                 options={options}
               />
             </div>
-            <div className="more-actions form-group">
-              <label className="mb-1">Upload image </label> <br />
-              {/* <input className="form control" type="file" onChange={handleChangeFile(event)} /> */}
-            </div>
+           
           </div>
+           <div className="more-actions form-group">
+              <label className="mb-1">Upload image </label> <br />
+              <input
+                className="form control"
+                type="file"
+                onChange={(event) => handleChangeFile(event)}
+              />
+            </div>
           <div>
-            <button className="btn btn-warning">Save</button>
+            <button className="btn btn-warning mt-3" onClick={handleSubmitQuiz}>
+              Save
+            </button>
           </div>
         </fieldset>
       </section>
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,55 +1,77 @@
 import axios from "../utils/axiosCustomize";
 const postCreateNewUser = (email, password, username, role, image) => {
-     //call api
-     const data  = new FormData();
-     data.append("email", email);
-     data.append("password", password);
-     data.append("username", username);
-     data.append("role", role);
-     data.append("userImage", image);
-     return axios.post("api/v1/participant", data);
-}
+  //call api
+  const data = new FormData();
+  data.append("email", email);
+  data.append("password", password);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return axios.post("api/v1/participant", data);
+};
 const getAllUser = () => {
-    return axios.get("api/v1/participant/all");
-}
-const putUpdateUser =  (id, username, role, image) => {
-    //call api
-    const data  = new FormData();
-    data.append("id", id);
-    data.append("username", username);
-    data.append("role", role);
-    data.append("userImage", image);
-    return axios.put(`api/v1/participant`, data);
-}
+  return axios.get("api/v1/participant/all");
+};
+const putUpdateUser = (id, username, role, image) => {
+  //call api
+  const data = new FormData();
+  data.append("id", id);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+  return axios.put(`api/v1/participant`, data);
+};
 const deleteUserApi = (idUser) => {
-    return axios.delete("api/v1/participant", {data: {id:idUser}});
-}
+  return axios.delete("api/v1/participant", { data: { id: idUser } });
+};
 const getUserWithPaginate = (page, limit) => {
-    return axios.get(`api/v1/participant?page=${page}&limit=${limit}`);
-}
-const postLogin = (userEmail,userPassword) =>{
-    return axios.post(`api/v1/login`, {
-        email: userEmail,
-        password: userPassword,
-        delay: 5000
-    });
-}
+  return axios.get(`api/v1/participant?page=${page}&limit=${limit}`);
+};
+const postLogin = (userEmail, userPassword) => {
+  return axios.post(`api/v1/login`, {
+    email: userEmail,
+    password: userPassword,
+    delay: 5000,
+  });
+};
 
-const postRegister = (email,username,password,role) =>{
-    return axios.post(`api/v1/register`, {email: email, username: username, password: password , role: role});
-}
+const postRegister = (email, username, password, role) => {
+  return axios.post(`api/v1/register`, {
+    email: email,
+    username: username,
+    password: password,
+    role: role,
+  });
+};
 
-const getQuizByUser = () =>{
-    return axios.get(`api/v1/quiz-by-participant`)
-}
-const getDataQuiz = (quizId) =>{
-    return axios.get(`api/v1/questions-by-quiz?quizId=${quizId}`)
-}
-const postSubmitQuiz = (data) =>{
-    return axios.post(`api/v1/quiz-submit`,{...data})
+const getQuizByUser = () => {
+  return axios.get(`api/v1/quiz-by-participant`);
+};
+const getDataQuiz = (quizId) => {
+  return axios.get(`api/v1/questions-by-quiz?quizId=${quizId}`);
+};
+const postSubmitQuiz = (data) => {
+  return axios.post(`api/v1/quiz-submit`, { ...data });
+};
+const postCreateNewQuiz = (description, difficulty, name, image) => {
+  const data = new FormData();
+  data.append("description", description);
+  data.append("name", name);
+  data.append("difficulty", difficulty);
+  data.append("quizImage", image);
+  return axios.post(`api/v1/quiz`,data)
+};
 
-}
-
-export { postCreateNewUser, getAllUser,putUpdateUser,
-    deleteUserApi,getUserWithPaginate,postLogin,postRegister,
-    getQuizByUser,getDataQuiz,postSubmitQuiz};
+export {
+  postCreateNewUser,
+  getAllUser,
+  putUpdateUser,
+  deleteUserApi,
+  getUserWithPaginate,
+  postLogin,
+  postRegister,
+  getQuizByUser,
+  getDataQuiz,
+  postSubmitQuiz,
+  postCreateNewQuiz,
+};
